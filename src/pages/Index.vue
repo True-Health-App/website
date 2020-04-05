c7efad70ef1ec02eeb042c5d2b40633fd928324a
Dev Landmax <dev@landmax.ca>
2020-04-05 14:38:08 -0600

    Initial Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- <q-page class="flex flex-center column"> -->
+  <q-page class="flex flex-center column">
+    <div class="fit q-pa-lg absolute" style="display:flex; flex-direction:column; align-items:center; overflow: scroll">
+      <img alt="True Health App logo" src="statics/icons/icon-512x512.png" style="max-width: 80%" />
+      <div class="q-ma-md" style="font-size: 2em">
+        <q>Let food be thy medicine and medicine be thy food.</q>
+      </div>
+      <div class="q-ma-md" style="font-size: 1.5em">
+        <p>
+          <b>Hippocrates</b> (c. 460 – c. 370 BC ) was a Greek doctor who is called the "father of medicine". He was the first person to teach that people got sick for
+          scientific reasons. Previously, people believed that disease was caused by angry gods.
+        </p>
+        <p>
+          Many of Hippocrates' writings are still important to doctors. For example, patient confidentiality, meaning that doctors can only discuss a patient with the patient themselves. Another of his ideas is to never knowingly lead a patient to suffering or death. These kinds of ideas are part of
+          medical ethics.
+        </p>
+        <p>
+          The Hippocratic Oath is named after him. This is a set of rules which doctors promise to obey. This is an example of a modern
+          <i>Hippocratic Oath</i> used at the
+          University of North Carolina School of Medicine.
+          I do solemnly swear by all I hold most sacred:
+        </p>
+        <ul>
+          <li>that I will be loyal to the profession of medicine and just to its members</li>
+          <li>that I will lead my life and practice my art with virtue and honor</li>
+          <li>that into whatsoever home I shall enter it shall be for the good of the sick and the well by the utmost of my power and that I will hold myself aloof from wrong and from corruption and from the tempting of others to vice</li>
+          <li>that I will exercise my art solely for the benefit of my patients, the relief of suffering, the prevention of disease and promotion of health, and I will give no drug and perform no act for an immoral purpose</li>
+          <li>that in the treatment of the sick, I will consider their well-being to be of a greater importance than their ability to compensate my services</li>
+          <li>that what I may see or hear in the course of treatment or even outside the treatment in regard to the lives of persons which is not fitting to be spoken, I will keep inviolably secret</li>
+          <li>that I will commit myself to a lifetime of continued learning of the art and science of medicine</li>
+          <li>these things I do promise and in proportion as I am faithful to this oath, may happiness and good repute be ever mine, but should I trespass and violate this oath, may the reverse be my lot.</li>
+        </ul>
+      </div>
+      <p>
+        <a href="https://simple.wikipedia.org/wiki/Hippocrates" target="_blank">From Wikipedia, the free encyclopedia</a>
+      </p>
+      <div class="full-width flex flex-center">
+        <img src="statics/bust.jpg" alt="Hippocrates Bust"  style="max-width: 100%">
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
